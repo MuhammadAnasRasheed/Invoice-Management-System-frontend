0bfe12edb652ebf01b18d1fabebd3665f3976072
MuhammadAnasRasheed <rasheedanas2000@gmail.com>
2026-05-14 14:15:35 +0500

    feat: initial React + TypeScript frontend setup with Tailwind CSS configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+    <h1 className="text-3xl font-bold text-blue-500 ">
+      Hello Tailwind
+    </h1>
+    </div>
+  )
+}
+
+export default App
+
