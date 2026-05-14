f59f512070b17962a31568285bd282dfc314e93b
MuhammadAnasRasheed <rasheedanas2000@gmail.com>
2026-05-14 16:45:57 +0500

    feat: add main layout with navbar and outlet for routed pages

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import { Navbar } from './Navbar';
+
+export const Layout = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navbar />
+      <main className="py-10">
+        <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+};
